944107c92db1c90f139fec2458a0c65e37705729
stevenspiel <stevenspiel@gmail.com>
2014-02-19 09:40:29 -0600

    worked on erb display and listing of shortened urls

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,10 +1,13 @@
 get '/' do
+  @urls = Url.all.map {|url| "#{url.shortened_url}  (#{url.url})"}
+
   # Look in app/views/index.erb
   erb :index
 end
 
 post '/urls' do
   @url = Url.create(params[:post])
+  @urls = Url.all.map {|url| "#{url.shortened_url}  (#{url.url})"}
   erb :index
 end
 
