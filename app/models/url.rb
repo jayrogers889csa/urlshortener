a50da42192f82593ca9a26cf89b296e6c8506950
stevenspiel <stevenspiel@gmail.com>
2014-02-19 13:31:38 -0600

    tentatively working validate?

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,7 +1,7 @@
 class Url < ActiveRecord::Base
 
-  validates :url, presence: true,
   before_create :shorten_url
+  validate :validate_url
 
   def shorten_url
     self.shortened_url = rand(99).to_s.concat(('a'..'z').to_a.sample).concat(('a'..'z').to_a.sample).concat(rand(99).to_s).concat(('a'..'z').to_a.sample)
@@ -16,20 +16,14 @@ class Url < ActiveRecord::Base
   end
 
   def validate_url
+    if self.url == ""
+      errors.add(:url, "You must enter a url into the form!")
     unless self.url.match(/\Ahttps?:\/\//)
       self.url = "http://".concat(self.url)
-
+    end
+    response = Net::HTTP.get_response(URI(self.url)
+    unless response.class == Net::HTTPSuccess || response.class == Net::HTTPOK then
+      errors.add(:url, "this is not a valid web address")
+    end
   end
 end
-
-
-
-=begin
-require 'uri'
-
-url.class == String
-! url.empty?
-URI.parse(url).kind_of?(URI::HTTP) == true
-
-net:http, open: uri , ask server if valid and check if GET return is within 200
-=end
